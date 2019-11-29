08bbc9e07ea6488b0cf624dd9f2d2567991bf4bf
wangliyang <364086070@qq.com>
2019-11-29 16:24:19 +0800

    xxx

--- app/src/main/java/com/bawei/wangliyang/MainActivity.java
+++ app/src/main/java/com/bawei/wangliyang/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+     //这是一行注释
+    //是一行注释
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
